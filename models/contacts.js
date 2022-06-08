f4b5bacb3ca6de5d383ccc088945797462349930
AleksandrVasilko <avasilko7@gmail.com>
2022-06-08 21:53:48 +0300

    edit server, app, routes, modrels

--- models/contacts.js
+++ models/contacts.js
@@ -1,79 +1,37 @@
-const fs = require('fs/promises');
-const path = require('path');
+const Joi = require('joi');
+const { Schema, model } = require('mongoose');
 const uuid = require('uuid');
 
-const contactsPath = path.join(__dirname, 'contacts.json');
-
-const listContacts = async () => {
-  const dataString = await fs.readFile(contactsPath, 'utf8');
-  const data = JSON.parse(dataString);
-  return (data);
-}
-
-const getContactById = async (contactId) => {
-  const type = (typeof contactId)
-    const allContacts = await listContacts();
-    allContacts.map(contact => {
-        const typeId = typeof contact.id
-        if(typeId !== type){
-            contactId = String(contactId)
-        }
-    });
-    const contact = allContacts.find(contact => contact.id === contactId)
-    return contact ? contact : null;
-}
-
-const removeContact = async (contactId) => {
-  const type = (typeof contactId)
-  const allNewContacts = await listContacts();
-  allNewContacts.map(contact => {
-    const typeId = typeof contact.id
-    if(typeId !== type){
-        contactId = String(contactId)
-    }
+const schema = new Schema({
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   });
-  const contactIndex = allNewContacts.findIndex(contact => contact.id === contactId);
-    if (contactIndex !== -1) { 
-        allNewContacts.splice(contactIndex,1)
-        await fs.writeFile(contactsPath, JSON.stringify(allNewContacts));
-    }    
-    console.log(allNewContacts)
-}
 
 
-const addContact = async (name, email, phone) => {
-  const newContact = {
-    id: uuid.v4(),
-    name: name,
-    email: email,
-    phone: phone,
-  }
-  const allContacts = await listContacts();
-  allContacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-  console.log(allContacts)
-}
+const schemaCreate = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+});
 
-const updateContact = async (contactId, name, email, phone) => {
-  const allContacts = await listContacts();
-  const contactIndex = allContacts.findIndex(contact => contact.id === contactId);
+const schemaPatch = Joi.object({
+    favorite: Joi.bool().required(),
+});
 
-  if(contactIndex !== -1){
-    allContacts[contactIndex].name = name;
-    allContacts[contactIndex].email = email;
-    allContacts[contactIndex].phone = phone;
-  
-    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
-    return allContacts[contactIndex];
-  } else {
-    return null;
-  }
-}
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+
+const Contact = model('contact', schema);
+
+module.exports = { Contact, schemaCreate, schemaPatch };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,78 +1,27 @@
 const express = require('express');
-const contacts = require('../../models/contacts');
 const router = express.Router();
+const createError = require ('../../errors');
+const {listContacts, getContactById, removeContact, addContact, updateContact, updateFavorite,} = require('../../controllers/contacts')
+
+const {schemaPatch, schemaCreate} = require('../../models/contact')
+const {validateRequest} = require('../../middlewares/validateRequest')
 
 
 router.use((req,res,next)=>{
-  console.log('in products');
+  console.log('in CONTACTS');
   next(); 
 })
 
-router.get('/', async (req, res, next) => {
-  //res.json({ message: 'template message' })
-    try{
-    const all = await contacts.listContacts();
-    res.json(all);
-  } catch(error){
-    res.status(500).json({massage: "Internal Server Error"})
-  }
-})
+router.get('/', listContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  //res.json({ message: 'template message' })
-  try{
-    const{contactId} = req.params;
-    const contact = await contacts.getContactById(contactId);
-    if(!contact){
-      res.status(404).json({"massage": "Not found"});
-    } else {
-      res.json(contact);
-    }
-  }catch (error){
-    res.status(500).json({massage: "Internal Server Error"})
-  }
-})
+router.get('/:contactId', getContactById);
 
-router.post('/', async (req, res, next) => {
-  //res.json({ message: 'template message' })
-  try{
-    const {name, email, phone} = req.body;
-    const contact = await contacts.addContact(name, email, phone);
-    res.status(201).json(contact);
-  } catch{
-    res.status(500).json({massage: "Internal Server Error"})
-  } 
-})
+router.post('/', validateRequest(schemaCreate), addContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  //res.json({ message: 'template message' })
-  try{
-    const{contactId} = req.params;
-    const contact = await contacts.removeContact(contactId);
-    if(!contact){
-      res.status(404).json({"massage": "NotFound"})
-    } else {
-      res.json({"massage": "contact deleted"});
-    }
-  } catch(error) {
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-})
+router.delete('/:contactId', removeContact)
 
-router.put('/:contactId', async (req, res, next) => {
-  //res.json({ message: 'template message' })
-  try{
-    const {name, email, phone} = req.body;
-    const {contactId} =req.params;
-    const contact = await contacts.updateContact(contactId, name, email, phone);
-    if(!contact){
-      res.status(404).json({"massage" : "Not Found"});
-    } else { 
-      res.json(contact);
-    }
-  }catch{
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+router.put('/:contactId', updateContact);
+
+router.patch('/:contactId/favorite', validateRequest(schemaPatch), updateFavorite);
 
 module.exports = router
