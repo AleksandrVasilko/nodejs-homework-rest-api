00fe9ca82f07d307bd1972b04179b479728b8229
AleksandrVasilko <avasilko7@gmail.com>
2022-06-14 22:11:36 +0300

    add model for user

---
+++ models/auth.js
@@ -0,0 +1,38 @@
+const { Schema, model } = required('mongoose');
+const Joi = require('joi');
+
+const schema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+})
+
+const schemaRegister = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.syting().required(),
+    subscription: Joi.string(),
+})
+
+const schemaLogin = Joi.object({
+    password: Joi.string().required(),
+    email: Joi.string().required(),
+})
+
+const User = model('user', schema);
+
+modelu.exports = {User, schemaRegister, schemaLogin}
--- models/contact.js
+++ models/contact.js
@@ -17,6 +17,10 @@ const schema = new Schema({
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    }
   });
 
 
